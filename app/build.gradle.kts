6be83d03d9106932e409db2442a5867ae3c71b36
decli <decli@qq.com>
2026-03-25 12:32:36 +0800

    chore: improve android release diagnostics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.decli.codehelper"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
         vectorDrawables {
             useSupportLibrary = true
         }
